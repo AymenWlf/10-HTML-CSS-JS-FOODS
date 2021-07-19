e49f9f485f8505076d8a0afa28e4936c75bc15e8
AymenWlf <84716044+AymenWlf@users.noreply.github.com>
2021-07-19 12:36:42 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/* NAVBAR */
+/* NAVBAR modif for test git pull */
 document.querySelector('.menu').addEventListener('click', () => {
         document.querySelectorAll('.target').forEach((item) => {
             item.classList.toggle('change');
